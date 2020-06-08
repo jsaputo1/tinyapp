d5eaffc75547c40c66445516c8cb985219e81c45
John Saputo <jsaputo1@gmail.com>
2020-06-08 23:11:06 +0000

    added post to generate short url code and add it to the urlDatabase object

--- express_server.js
+++ express_server.js
@@ -43,8 +43,11 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const longUrl = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longUrl;
+  console.log(urlDatabase);
+  res.send("ok"); //
 });
 
 app.get("/urls/:shortURL", (req, res) => {
