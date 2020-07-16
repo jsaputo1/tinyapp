b97f67081648650f200900a8f7b4fcf739bb84c3
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-07-16 13:58:18 -0400

    changed port to process.env

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = process.env.PORT || 5000;
 app.set("view engine", "ejs");
 
 //Helper Functions
